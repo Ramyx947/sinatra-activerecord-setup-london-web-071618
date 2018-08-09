75505ee12f4bd747e0f6725f541901dd81e20f4f
github username <github email address>
2018-08-09 10:45:26 +0100

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,16 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+### Connecting to the Database
+      configure :development do
+        set :database, 'sqlite3:db/database.db'
+      end
+ # This sets up a connection to a sqlite3 database named "database.db", located in a folder called "db." If we wanted our `.db` file to be called `dogs.db`, we could simply change the name of this file:
+  #
+  # ```ruby
+  # configure :development do
+  #   set :database, 'sqlite3:db/dogs.db'
+  # end
+
+
 require './app'
